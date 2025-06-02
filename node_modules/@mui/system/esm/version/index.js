a17bbc3073f801558573fda84f9bb5efaef4580a
Povea <107443470+DPoveaa@users.noreply.github.com>
2025-06-02 11:43:57 -0300

    FIx

---
+++ node_modules/@mui/system/esm/version/index.js
@@ -0,0 +1,6 @@
+export const version = "6.4.11";
+export const major = Number("6");
+export const minor = Number("4");
+export const patch = Number("11");
+export const prerelease = undefined;
+export default version;
